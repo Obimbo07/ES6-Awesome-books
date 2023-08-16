a109db7cc8bcec074aedfde202848d134bfc933d
Austin Obimbo <118368849+Obimbo07@users.noreply.github.com>
2023-08-16 14:02:19 +0300

    luxon module and project ES6 scripts

---
+++ index.js
@@ -0,0 +1,23 @@
+import updateDom from './modules/updateDOM.js';
+import BookShelf from './modules/books.js';
+import  Date  from './modules/date.js';
+
+const container = document.querySelector('#bookList');
+const form = document.querySelector('#addForm');
+const submit = form.querySelector('button');
+
+submit.addEventListener('click', (event) => {
+  event.preventDefault();
+  const author = form.authorInput.value;
+  const title = form.titleInput.value;
+  const book = new BookShelf(container, title, author);
+  book.updateStorage();
+  book.renderBooks();
+  form.authorInput.value = '';
+  form.titleInput.value = '';
+  event.submit();
+});
+
+const display = new BookShelf(container);
+display.renderBooks();
+updateDom();
